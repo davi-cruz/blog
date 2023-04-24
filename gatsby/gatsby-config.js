9de0b0b88142a2802b115e9366a96aca47feda48
Davi Cruz <eu@davicruz.com>
2023-04-24 13:12:44 -0300

    Gatsby Blog starter

---
+++ gatsby/gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `Davi Cruz`,
+    author: `Davi Cruz`,
+    description: `Just another cybersecurity prfessional blog`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/zerahzurc`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/davi-cruz`,
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+}
