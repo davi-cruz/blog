017a1a9f2cfcb55fea6cb4c271fd12e16eafdb0c
Davi Cruz <eu@davicruz.com>
2023-07-14 13:09:54 -0300

    Finished Gatsby Tutorial Part 4

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -4,9 +4,17 @@
 module.exports = {
   siteMetadata: {
     siteUrl: `https://davicruz.com`,
+    title: "Davi Cruz",	
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 }
