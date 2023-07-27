f8c7d03f5f5744fd96af24337380bc2e3881a76b
Davi Cruz <eu@davicruz.com>
2023-07-27 16:56:00 -0300

    Removed old Gatsby Blogs

--- gatsby-multilang/gatsby-browser.jsx
+++
@@ -1,9 +0,0 @@
-import { ThemeProvider } from './src/components/ContextProviders/ThemeProvider';
-import { GlobalStyle } from './src/components/Layout/sharedStyles/globalStyles';
-
-export const wrapPageElement = ({ element }) => (
-  <>
-    <GlobalStyle />
-    <ThemeProvider>{element}</ThemeProvider>
-  </>
-);
