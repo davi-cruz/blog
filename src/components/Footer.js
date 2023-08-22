8e92b9c9b92af8ef5a7208837bc6e352ffb4da53
Davi Cruz <eu@davicruz.com>
2023-08-22 13:26:07 -0300

    Fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-// import { StaticImage } from "gatsby-plugin-image";
+import Logo from "./Logo";
 import { Box, Container, Typography, Paper, Image} from "@mui/material";
 import MuiLink from "@mui/material/Link";
 
@@ -24,7 +24,7 @@ const Footer = () => {
           }}
         >
           <div>
-            {/* <StaticImage src="../images/logo.png" alt="Logo" /> */}
+            <Logo/>
           </div>
         </Box>
 
---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Icon } from "@mui/material";
+
+const Logo = () => {
+  return (
+  <Icon>
+    <img src="../images/logo.svg" alt="Davi Cruz" width="100" height="100" />
+  </Icon>
+  );
+};
+
+export default Logo;
